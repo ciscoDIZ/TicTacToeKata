b4a7e9be152c6c42d2eed556043a5f44eb242c0d
francisco de asís domínguez iceta <ciscoadiz@gmail.com>
2024-05-03 10:58:59 +0100

    Green - a player can take a field if not already taken

--- TicTacToeKata.Core/TicTacToe.cs
+++ TicTacToeKata.Core/TicTacToe.cs
@@ -9,12 +9,16 @@ public class TicTacToe
         _board = new string[3][];
         for (int i = 0; i < _board.Length; i++)
         {
-            _board[i] = new string[3];
+            _board[i] = ["", "", ""];
         }
     }
 
     public string[][] Play(int row, int column, string player)
     {
+        if (_board[row][column].Equals(""))
+        {
+            _board[row][column] = player;
+        }
         return _board;
     }
 }
