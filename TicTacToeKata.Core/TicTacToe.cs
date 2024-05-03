1308ba6b91f465fb3bd80e4562e4cc84623ee0b1
francisco de asís domínguez iceta <ciscoadiz@gmail.com>
2024-05-03 09:20:11 +0100

    Red - a game has nine fields in a 3x3 grid

---
+++ TicTacToeKata.Core/TicTacToe.cs
@@ -0,0 +1,11 @@
+namespace TicTacToeKata;
+
+public class TicTacToe
+{
+    private readonly string[][] _board = null!;
+
+    public string[][] Play(int row, int column, string player)
+    {
+        return _board;
+    }
+}
---
+++ TicTacToeKata.Tests/TicTacToeShould.cs
@@ -0,0 +1,31 @@
+namespace TicTacToe.Tests;
+
+public class TicTacToeShould
+{
+    /*
+     * a game is over when all fields in a row are taken by a player.
+     * players take turns taking fields until the game is over.
+     * a game is over when all fields in a diagonal are taken by a player.
+     * a game is over when all fields are taken.
+     * there are two players in the game (X and O).
+     * a game has nine fields in a 3x3 grid.
+     * a game is over when all fields in a column are taken by a player.
+     * a player can take a field if not already taken.
+     * */
+
+    [Fact]
+    public void have_a_board_with_nine_fields()
+    {
+        // Arrange
+        var ticTacToe = new TicTacToeKata.TicTacToe();
+        var expected = 9;
+        
+        // Act
+        var actual = ticTacToe.Play(0, 0, "X").Length * 
+                     ticTacToe.Play(0, 0, "X")[0].Length;
+        
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+   
+}
